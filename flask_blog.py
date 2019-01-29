9dfc803fd8d04e28c5f34fe45413b5f322655ab0
@dorothykiz1 <kizdorothy@gmail.com>
2019-01-29 12:10:01 +0300

    Modifies flask_blog file

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template
+from forms import RegistrationForm,LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='79d64c304c964d793be61e7fc7696a9b'
+# secret key for this app
 
 posts =[
     {
@@ -24,6 +28,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About page')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
