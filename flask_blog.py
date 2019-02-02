59a097cd365176ce49d24cc45532970676a07ec7
@dorothykiz1 <kizdorothy@gmail.com>
2019-02-02 20:19:49 +0300

    modifies flask_blog.py file

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,40 @@
-from flask import Flask,render_template,flash,redirect,url_for
-from forms import RegistrationForm,LoginForm
+from datetime import datetime
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='79d64c304c964d793be61e7fc7696a9b'
+app.config['SECRET_KEY' = '79d64c304c964d793be61e7fc7696a9b'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+# sqlite /// specify the relative path 
 # secret key for this app
 # config values in our application app.config
+db = SQLAlchemy(app)
+
+
+# user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    # backref-adding another column if we have apost we can get ts author
+
+#    this method specifies how the object will be printed out
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String(100),unique=True,nullable=False)
+    date_posted =db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    content=db.Column(db.Text,nullable=False)
+    user_id =db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title }','{self. date_posted}')"
 
 posts =[
     {
@@ -48,10 +78,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.Email.data == 'kizdorothy@gmail.com' and form.Password.data =='password':
-            flash(f'You have been logged in','success')
+            flash('You have been logged in','success')
             return redirect(url_for('home'))
         else:
-            flash(f'Login unsuccessful.please try again','danger')
+            flash('Login unsuccessful.please try again','danger')
         return render_template('login.html',title='login',form='form')
 
             
