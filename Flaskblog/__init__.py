dff6c804431f4cc3f8f4878a6cf0c47493f58262
@dorothykiz1 <kizdorothy@gmail.com>
2019-02-05 09:56:40 +0300

    Modify package

--- Flaskblog/__init__.py
+++ Flaskblog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '79d64c304c964d793be61e7fc7696a9b'
@@ -10,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # config values in our application app.config
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from Flaskblog import routes
