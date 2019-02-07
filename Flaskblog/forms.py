7b61269458c833ea3a593afc1c8942c5be945bf7
@dorothykiz1 <kizdorothy@gmail.com>
2019-02-07 13:18:03 +0300

    modify files

--- Flaskblog/forms.py
+++ Flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField ,PasswordField,SubmitField,BooleanField,ValidationError
 from wtforms.validators import DataRequired ,Length,Email,EqualTo
@@ -37,6 +38,7 @@ class UpdateAccountForm(FlaskForm):
      
     username = StringField('Username',validators= [DataRequired(), 
                             Length(min=2, max=20)])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     email = StringField('Email',validators= [DataRequired(), Email()])
     submit = SubmitField('Update')
     #function for validating whether or not the username is already there
--- Flaskblog/routes.py
+++ Flaskblog/routes.py
@@ -84,10 +84,11 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+@app.route('/account',methods=["GET","POST"])
 @login_required
 def account():
     form = UpdateAccountForm()
     image_file = url_for('static', filename ='profile_pics/'+ current_user.image_file)
-    return render_template('account.html', title='Account',image_file = image_file,form = form)
+    return render_template('account.html',
+                             title='Account',image_file = image_file,form = form)
  
